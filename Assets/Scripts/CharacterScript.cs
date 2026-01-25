291470bd023f614f67ebc4e390770124f9a62b3e
Dakspieth <jrb.games09@gmail.com>
2026-01-25 17:09:23 -0500

    started making duplicator

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -4,13 +4,21 @@ public class CharacterScript : MonoBehaviour
 {
     public float health;
     public float speed;
+    [Header("Sword")]
     public bool hasSword; // spinning sword that does damage on hit
     public int swordDamage;
     public float swordSpeed;
     public GameObject swordPrefab;
+    [Header("Unarmed")]
     public bool unarmed; // does damage when ball hits ball
     public int unarmedDamage;
     public bool unarmedSpeedOnHit;
+    [Header("Duplicator")]
+    GameObject ballPrefab;
+    public bool duplicator;
+    public int duplicateHealth;
+    public float duplicateSize;
+    public int duplicateDamage;
 
     float angle;
     Rigidbody2D rb;
@@ -25,6 +33,14 @@ public class CharacterScript : MonoBehaviour
         {
             Instantiate(swordPrefab, gameObject.transform);
         }
+        if (duplicator)
+        {
+            ballPrefab = Instantiate(gameObject, transform.position, Quaternion.identity); ;
+            ballPrefab.GetComponent<CharacterScript>().health = duplicateHealth;
+            ballPrefab.transform.localScale *= duplicateSize;
+            ballPrefab.layer = 6;
+        }
+        
     }
 
     void Update()
@@ -50,15 +66,38 @@ public class CharacterScript : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D col)
     {
-        if (unarmed && col.gameObject.tag != gameObject.tag && col.gameObject.tag != "Border") // rules for on collision if unarmed is enabled
+        if (col.gameObject.tag != gameObject.tag && col.gameObject.tag != "Border") // rules for on collision if unarmed is enabled
         {
-            col.gameObject.GetComponent<CharacterScript>().health -= unarmedDamage;
-            if (unarmedSpeedOnHit)
+            if (unarmed)
             {
-                speed += Mathf.Pow(10,-(speed+0.7f));
-                rb.mass = rb.mass>0.1f ? rb.mass/1.5f : 0.1f;
+                col.gameObject.GetComponent<CharacterScript>().health -= unarmedDamage;
+                if (unarmedSpeedOnHit)
+                {
+                    speed += Mathf.Pow(10,-(speed+0.7f));
+                    rb.mass = rb.mass>0.1f ? rb.mass/1.5f : 0.1f;
+                }
             }
             
+            if(duplicator)
+            {
+                if(gameObject.layer == 0)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up * Random.Range(-1, 1) + transform.right * Random.Range(-1, 1), 1);
+                    while (hit)
+                    {
+                        hit = Physics2D.Raycast(transform.position, transform.up * Random.Range(-1, 1) + transform.right * Random.Range(-1, 1), 1);
+                        print("not work");
+                    }
+                    Instantiate(ballPrefab, hit.point, Quaternion.identity);
+                    
+                    print("yay");
+                }
+                if (gameObject.layer == 6)
+                {
+                col.gameObject.GetComponent<CharacterScript>().health -= unarmedDamage;
+                }
+            }
+
         }
         angle = (rb.linearVelocity.x)/-7.5f;
     }
