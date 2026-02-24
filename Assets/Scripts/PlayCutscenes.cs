185f21282037d1948a23ac279106383ad567faaa
Dakspieth <jrb.games09@gmail.com>
2026-02-23 22:12:46 -0500

    not completely finished but all there is is going between levels and balancing matches basically

--- Assets/Scripts/PlayCutscenes.cs
+++ Assets/Scripts/PlayCutscenes.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class PlayCutscenes : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public VideoClip[] clips;
+    public Button opponentBtn;
+    public Button cutsceneSkip;
+    bool buttonDestroyed = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    {        
+        opponentBtn.interactable = false;
+
         if(clips.Length > 1)
         {
         videoPlayer.loopPointReached += playNextClip;            
@@ -24,6 +30,10 @@ public class PlayCutscenes : MonoBehaviour
         videoPlayer.clip = clips[1];
         videoPlayer.loopPointReached -= playNextClip;
         videoPlayer.loopPointReached += endPlayback;
+        if(!buttonDestroyed)
+        {
+            Destroy(cutsceneSkip.gameObject);
+        }
         videoPlayer.Play();
     }
 
@@ -31,5 +41,13 @@ public class PlayCutscenes : MonoBehaviour
     {
         videoPlayer.Stop();
         videoPlayer.enabled = false;
+        opponentBtn.interactable = true;
+    }
+
+    public void skipClick()
+    {
+        videoPlayer.frame = (long)videoPlayer.frameCount-1;
+        buttonDestroyed = true;
+        Destroy(cutsceneSkip.gameObject);
     }
 }
